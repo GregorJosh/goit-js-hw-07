1ad4555fabccb88f8a854cad5102ceb20dad4e52
GregorJosh <grzegorz.jozw@gmail.com>
2023-06-02 21:50:01 +0200

    Init

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+import basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/+esm";
+
+const galleryULElement = document.querySelector("ul.gallery");
+const onEscapeKeyEvent = (event) => {
+  event.preventDefault();
+
+  if (event.key === "Escape" && basicLightboxInstance) {
+    basicLightboxInstance.close();
+    basicLightboxInstance = null;
+
+    galleryULElement.removeEventListener("keyup", onEscapeKeyEvent);
+  }
+};
+const onGalleryItemClick = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName === "IMG") {
+    const { alt } = event.target;
+    const { href } = event.target.parentElement;
+
+    basicLightboxInstance = basicLightbox.create(`<img src="${href}" alt="${alt}" />`);
+    basicLightboxInstance.show();
+
+    galleryULElement.addEventListener("keyup", onEscapeKeyEvent);
+  }
+};
+
+let basicLightboxInstance = null;
+
+galleryULElement.insertAdjacentHTML(
+  "afterbegin",
+  galleryItems
+    .map(
+      ({ preview, original, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+            </a>
+        </li>
+    `
+    )
+    .join("")
+);
+
+galleryULElement.addEventListener("click", onGalleryItemClick);
---
+++ js/02-lightbox.js
@@ -0,0 +1,21 @@
+import { galleryItems } from "./gallery-items.js";
+
+document.querySelector("ul.gallery").insertAdjacentHTML(
+  "afterbegin",
+  galleryItems
+    .map(
+      ({ preview, original, description }) => `
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `
+    )
+    .join("")
+);
+
+new SimpleLightbox("ul.gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
